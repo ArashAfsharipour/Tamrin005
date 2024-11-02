2b69d084be754d7f60f78ee445998a6eda6300ae
AFSHAR <AFSHAR@DESKTOP-H088APG>
2024-11-02 23:37:55 +0330

    Add project files.

---
+++ Tamrin5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Yek carakter vared konid: ");
+        char input = Console.ReadKey().KeyChar;
+
+        Console.WriteLine($"\nshoma yek adad vared kardid: {input}");
+    }
+}
